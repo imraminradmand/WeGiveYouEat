c88c62e2d061ee5e0aeefc5e9f9e0cb8d6427cd0
danzola01 <danzola01@gmail.com>
2022-10-04 17:42:11 -0600

    File to test if linter is working

---
+++ Linter_test/Main.java
@@ -0,0 +1,15 @@
+// Test to see if the linter works
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!" );
+
+        x=1;
+        y = 2;
+
+        x+y = z;
+
+        system.out.println(z);
+
+}
+
+}
