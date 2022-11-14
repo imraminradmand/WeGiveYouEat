c4ff13b6177f5b8a0c8f888a5790cb974abb279e
Ramin Radmand <69999501+imraminradmand@users.noreply.github.com>
2022-11-13 20:41:39 -0700

    some parts of post page

--- screens/PostScreen.tsx
+++ screens/PostScreen.tsx
@@ -1,8 +1,116 @@
-import { View, Dimensions, StyleSheet, Text, SafeAreaView } from "react-native";
+import React from "react";
+import { useState, useEffect } from "react";
+import {
+  View,
+  Dimensions,
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  Image,
+  Button,
+  TextInput,
+} from "react-native";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import Ionicons from "react-native-vector-icons/Ionicons";
+
+import * as ImagePicker from "expo-image-picker";
+import CustomButton from "../components/CustomButton";
+import InputField from "../components/InputField";
+import MultilineInput from "../components/MultilineInput";
+
+const testArr = [
+  "file:///Users/raminradmand/Library/Developer/CoreSimulator/Devices/5EFEF0B8-9CD5-4A8A-8F63-0FBEEC063C1E/data/Containers/Data/Application/7D38E662-2F99-4868-8812-032FC3CB7157/Library/Caches/ExponentExperienceData/%2540raminradmand%252FWeGiveYouEat/ImagePicker/447FB97F-791A-428F-8365-BEE7E253FAF8.jpg",
+  "file:///Users/raminradmand/Library/Developer/CoreSimulator/Devices/5EFEF0B8-9CD5-4A8A-8F63-0FBEEC063C1E/data/Containers/Data/Application/7D38E662-2F99-4868-8812-032FC3CB7157/Library/Caches/ExponentExperienceData/%2540raminradmand%252FWeGiveYouEat/ImagePicker/B367DF27-5431-4A5C-A5A8-EC1048CEC065.jpg",
+];
+
 const PostScreen = () => {
+  const [hasPermissions, setPermissions] = useState(false);
+  const [image, setImage] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const galleryStatus = await ImagePicker.getMediaLibraryPermissionsAsync();
+      setPermissions(galleryStatus.status === "granted");
+    })();
+  }, []);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsMultipleSelection: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      let imageArr: any = [];
+      result.assets.forEach((asset) => {
+        imageArr.push(asset.uri);
+      });
+      console.log(imageArr);
+      setImage(imageArr);
+    }
+  };
   return (
     <SafeAreaView style={styles.container}>
-      <Text>POST PAGE</Text>
+      <View style={{ top: 20 }}>
+        <Text style={styles.header}>Create New Post</Text>
+      </View>
+
+      <View style={styles.imageCarousel}>
+        <CustomButton label="Upload Image(s)" onPress={pickImage} />
+      </View>
+
+      <View style={styles.body}>
+        <InputField
+          label="Address"
+          icon={
+            <MaterialIcons
+              name="home"
+              size={20}
+              color="#666"
+              style={{ marginRight: 5 }}
+            />
+          }
+          keyboardType="email-address"
+        />
+        <InputField
+          label="Phone"
+          icon={
+            <MaterialIcons
+              name="phone"
+              size={20}
+              color="#666"
+              style={{ marginRight: 5 }}
+            />
+          }
+          keyboardType="email-address"
+        />
+        <TextInput
+          style={{
+            height: 80,
+            width: 300,
+            padding: 10,
+            borderColor: "grey",
+            borderWidth: 1,
+            borderRadius: 10,
+          }}
+          multiline={true}
+          placeholder="Description"
+        />
+
+        <View style={{ alignContent: "center", top: 15 }}>
+          <Text
+            style={{
+              alignSelf: "center",
+              fontFamily: "Roboto-Regular",
+              fontSize: 18,
+            }}
+          >
+            Category
+          </Text>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
@@ -15,10 +123,24 @@ const styles = StyleSheet.create({
     width: Dimensions.get("screen").width,
     height: Dimensions.get("screen").height,
     justifyContent: "flex-start",
-    alignItems: "stretch",
+    alignItems: "center",
+    backgroundColor: "#fff49b",
   },
   header: {
     fontSize: 30,
     alignItems: "center",
+    fontFamily: "Roboto-Regular",
+  },
+  image: {
+    width: "100%",
+    height: 300,
+    resizeMode: "contain",
+  },
+  imageCarousel: {
+    top: 200,
+  },
+  body: {
+    top: 250,
+    width: 300,
   },
 });
