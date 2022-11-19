582a5b86ef0d161de5a11454284f922b47ec1137
Ramin Radmand <69999501+imraminradmand@users.noreply.github.com>
2022-11-19 11:16:28 -0700

    Revert "Issue#15   create bottom navigation bar" (#42)

--- App.tsx
+++ App.tsx
@@ -1,34 +1,20 @@
-import React, { useCallback, useEffect, useState } from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { Text, View } from "react-native";
-import * as SplashScreen from "expo-splash-screen";
-import MainScreen from "./screens/MainScreen";
-import PostScreen from "./screens/PostScreen";
-import LoginScreen from "./screens/LoginScreen";
-import { useFonts } from "expo-font";
-import SignUpPage from "./screens/SignUpScreen";
-import BottomBar from "./navigation/BottomBar";
-
-SplashScreen.preventAutoHideAsync();
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
-  });
-
-  const onLayoutRootView = useCallback(async () => {
-    if (fontsLoaded) {
-      await SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded]);
-
-  if (!fontsLoaded) {
-    return null;
-  }
-
   return (
-    <View>
-      <Text>Test</Text>
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
