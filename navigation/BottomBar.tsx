21e08a08c26cd04e07c31305f513c6667b0edf24
Ramin Radmand <69999501+imraminradmand@users.noreply.github.com>
2022-11-19 20:49:57 -0700

    authfor posting page completed

--- navigation/BottomBar.tsx
+++ navigation/BottomBar.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { getFocusedRouteNameFromRoute } from "@react-navigation/native";
+import { auth } from "../firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
 import MainScreen from "../screens/MainScreen";
 import PostScreen from "../screens/PostScreen";
@@ -8,10 +9,28 @@ import AuthStack from "../navigation/AuthNavigator";
 
 import Feather from "react-native-vector-icons/Feather";
 import { View, StyleSheet } from "react-native";
+import LoginScreen from "../screens/LoginScreen";
 
 const Tab = createBottomTabNavigator();
 
 const BottomBar = () => {
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  function handleAuthChange(user: any) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = onAuthStateChanged(auth, (user) => {
+      handleAuthChange(user);
+    });
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) return null;
+
   return (
     <Tab.Navigator
       screenOptions={{
@@ -31,40 +50,69 @@ const BottomBar = () => {
         tabBarActiveTintColor: "yellow",
       }}
     >
-      <Tab.Screen
-        name="Home"
-        component={MainScreen}
-        options={() => ({
-          tabBarIcon: ({ color, size }) => (
-            <View style={styles.iconView}>
-              <Feather name="map" color={color} size={size} />
-            </View>
-          ),
-        })}
-      />
-      <Tab.Screen
-        name="Post"
-        component={PostScreen}
-        options={{
-          tabBarBadgeStyle: { backgroundColor: "yellow" },
-          tabBarIcon: ({ color, size }) => (
-            <View style={styles.iconView}>
-              <Feather name="plus-circle" color={color} size={50} />
-            </View>
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="LoginFlow"
-        component={AuthStack}
-        options={{
-          tabBarIcon: ({ color, size }) => (
-            <View style={styles.iconView}>
-              <Feather name="user" color={color} size={size} />
-            </View>
-          ),
-        }}
-      />
+      {user ? (
+        <>
+          <Tab.Screen
+            name="Home"
+            component={MainScreen}
+            options={() => ({
+              tabBarIcon: ({ color, size }) => (
+                <View style={styles.iconView}>
+                  <Feather name="map" color={color} size={size} />
+                </View>
+              ),
+            })}
+          />
+          <Tab.Screen
+            name="Post"
+            component={PostScreen}
+            options={{
+              tabBarBadgeStyle: { backgroundColor: "yellow" },
+              tabBarIcon: ({ color, size }) => (
+                <View style={styles.iconView}>
+                  <Feather name="plus-circle" color={color} size={50} />
+                </View>
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="LoginFlow"
+            component={AuthStack}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <View style={styles.iconView}>
+                  <Feather name="user" color={color} size={size} />
+                </View>
+              ),
+            }}
+          />
+        </>
+      ) : (
+        <>
+          <Tab.Screen
+            name="Home"
+            component={MainScreen}
+            options={() => ({
+              tabBarIcon: ({ color, size }) => (
+                <View style={styles.iconView}>
+                  <Feather name="map" color={color} size={size} />
+                </View>
+              ),
+            })}
+          />
+          <Tab.Screen
+            name="LoginFlow"
+            component={AuthStack}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <View style={styles.iconView}>
+                  <Feather name="user" color={color} size={size} />
+                </View>
+              ),
+            }}
+          />
+        </>
+      )}
     </Tab.Navigator>
   );
 };
