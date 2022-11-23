120f05eb4650230ab7f7715d2a9a87d899f25bef
Ramin Radmand <69999501+imraminradmand@users.noreply.github.com>
2022-11-22 23:17:02 -0700

    created all new api endpoints

---
+++ api/app.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.set("Content-Type", "application/json");
+  next();
+});
+
+const startServer = async (_) => {
+  const database = require("./src/db");
+  let db = await database.setup();
+
+  const routes = require("./src/routes");
+  routes.register(app, db);
+
+  const PORT = process.env.PORT || 8080;
+  const server = app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}...`);
+  });
+  process.on("unhandledRejection", (err) => {
+    console.error(err);
+    throw err;
+  });
+
+  return server;
+};
+
+startServer();
---
+++ api/src/db.js
@@ -0,0 +1,20 @@
+const mysql = require("promise-mysql");
+require("dotenv").config();
+
+const connectToDatabase = async () => {
+  return await mysql.createPool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+
+    connectionLimit: 5,
+    connectTimeout: 10000,
+    acquireTimeout: 10000,
+    waitForConnections: true,
+    queueLimit: 0,
+  });
+};
+
+module.exports.setup = connectToDatabase;
