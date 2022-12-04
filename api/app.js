585aaf59b7c567fed32ec10c800798a7a3171a93
Ramin Radmand <69999501+imraminradmand@users.noreply.github.com>
2022-12-04 00:34:10 -0700

    413 issue resolved

--- api/app.js
+++ api/app.js
@@ -2,13 +2,11 @@
 
 const express = require("express");
 const app = express();
-app.use(express.json());
-app.use(bodyParser.json({
-  limit: '10mb'
+app.use(express.urlencoded({
+  extended: true
 }));
-app.use(bodyParser.urlencoded({
-  extended: true,
-  limit: '10mb'
+app.use(express.json({
+  limit: '50mb'
 }));
 
 app.use((req, res, next) => {
@@ -24,7 +22,7 @@ const startServer = async (_) => {
   routes.register(app, db);
 
 
-  const PORT = process.env.PORT || 8080;
+  const PORT = process.env.PORT || 8081;
   const server = app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}...`);
   });
